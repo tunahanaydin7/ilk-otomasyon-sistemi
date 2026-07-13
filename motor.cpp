ce91b26ecf97e539b3a19dca8eca90bb3e6e2191
tunahanaydin7 <tunahan_aydin@outlook.com>
2026-07-13 12:36:41 +0300

    Ilk otonom sistem terminal uzerinden basariyla ayaga kaldirildi

---
+++ motor.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Sistem Baslatiliyor..." << std::endl;
+    for(int i = 1; i <= 3; i++) {
+        std::cout << "Motor " << i << " aktif edildi ve stabil." << std::endl;
+    }
+    std::cout << "DevOps mimarisi basariyla kuruldu!" << std::endl;
+    return 0;
+}
